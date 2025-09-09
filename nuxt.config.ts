9b39bcb24776149dbea3da3fc8e7ea1cf7752ec0
Fubon - Website Design & Frontend Development <43171309+thefubon@users.noreply.github.com>
2025-09-09 13:35:55 +0300

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: false },
+  css: ['~/assets/css/tailwind.css'],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+  modules: [
+    'shadcn-nuxt'
+  ],
+  shadcn: {
+    prefix: '',
+    componentDir: './app/components/ui'
+  }
+})
