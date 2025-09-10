1b11d997c11ae775f589bd59bafc0aabc2de5111
Fubon - Website Design & Frontend Development <43171309+thefubon@users.noreply.github.com>
2025-09-10 11:10:44 +0300

    upd

--- app/components/blocks/CardsThree.vue
+++ app/components/blocks/CardsThree.vue
@@ -3,7 +3,7 @@ import { Button } from '@/components/ui/button'
 
 const components: { title: string, href: string, description: string, image: string, position: string }[] = [
   {
-    title: "Одна платформа вместо 6+ отдельных сервисов",
+    title: "Одна платформа вместо&nbsp;6+ отдельных сервисов",
     href: "#",
     description:
       "Все инструменты для коммуникации и организации рабочего процесса собраны в одном пространстве – без переключений и потерь данных.",
@@ -19,7 +19,7 @@ const components: { title: string, href: string, description: string, image: str
     position: ""
   },
   {
-    title: "Простой интерфейс, не требующий обучения",
+    title: "Простой интерфейс, не&nbsp;требующий обучения",
     href: "#",
     description:
       "Интуитивно понятная среда – сотрудники начинают пользоваться сервисами сразу, без долгого онбординга и обучения.",
@@ -32,16 +32,14 @@ const components: { title: string, href: string, description: string, image: str
 <template>
   <div class="container flex flex-col gap-6 md:gap-8 lg:gap-10 2xl:gap-12">
     <div v-for="component in components" :key="component.title"
-      class="bg-slate-100 rounded-3xl flex flex-col-reverse lg:flex-row items-center p-8 md:p-10"
+      class="bg-slate-100 rounded-3xl flex flex-col-reverse lg:flex-row items-center gap-10 xl:gap-12 p-8 md:p-10"
       :class="component.position">
 
       <div class="space-y-10 flex-1">
         <div class="space-y-4">
-          <h3>{{ component.title }}</h3>
+          <h3 v-html="component.title"></h3>
 
-          <p class="line-clamp-3 text-muted-foreground text-base md:text-lg">
-            {{ component.description }}
-          </p>
+          <p class="line-clamp-3 text-muted-foreground text-base md:text-lg" v-html="component.description"></p>
         </div>
 
         <div>
@@ -49,8 +47,8 @@ const components: { title: string, href: string, description: string, image: str
         </div>
       </div>
 
-      <div>
-        <NuxtImg :src="component.image" :alt="component.title" class="flex-1" />
+      <div class="flex-1">
+        <NuxtImg :src="component.image" :alt="component.title" class="w-full" />
       </div>
 
     </div>
