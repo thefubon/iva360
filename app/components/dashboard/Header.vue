48601fbbe0e7e3d1005c74fee1cc63730a19a128
Fubon - Website Design & Frontend Development <43171309+thefubon@users.noreply.github.com>
2025-09-12 18:23:31 +0300

    upd

--- app/components/dashboard/Header.vue
+++ app/components/dashboard/Header.vue
@@ -2,6 +2,13 @@
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { Button } from "@/components/ui/button";
 import { ShoppingBag, Ticket, MonitorSmartphone } from 'lucide-vue-next';
+import { useBreakpoints } from '@vueuse/core';
+
+const breakpoints = useBreakpoints({
+  mobile: 768,
+});
+
+const isMobile = breakpoints.smaller('mobile');
 </script>
 
 <template>
@@ -11,13 +18,13 @@ import { ShoppingBag, Ticket, MonitorSmartphone } from 'lucide-vue-next';
     </div>
 
     <div class="flex items-center gap-4">
-      <Button variant="outline" asChild>
+      <Button variant="outline" :size="isMobile ? 'icon' : 'default'" asChild>
         <NuxtLink to="#" class="flex items-center gap-2">
           <MonitorSmartphone class="size-6 stroke-[1.5]" /> <span class="hidden md:block">Приложение</span>
         </NuxtLink>
       </Button>
 
-      <Button variant="secondary" asChild>
+      <Button variant="secondary" :size="isMobile ? 'icon' : 'default'" asChild>
         <NuxtLink to="#" class="flex items-center gap-2">
           <Ticket class="size-6 stroke-[1.5]" /> <span class="hidden md:block">Промокод</span>
         </NuxtLink>
