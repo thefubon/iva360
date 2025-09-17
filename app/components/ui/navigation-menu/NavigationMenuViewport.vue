6ef66faf3ee7ddb3768449fef4d0d62469d4bb56
Fubon - Website Design & Frontend Development <43171309+thefubon@users.noreply.github.com>
2025-09-17 19:50:15 +0300

    upd

--- app/components/ui/navigation-menu/NavigationMenuViewport.vue
+++ app/components/ui/navigation-menu/NavigationMenuViewport.vue
@@ -19,10 +19,10 @@ const forwardedProps = useForwardProps(delegatedProps)
 </script>
 
 <template>
-  <div class="absolute top-full inset-x-0 isolate z-60">
+  <div class="absolute top-full inset-x-0 isolate z-60 border-t">
     <NavigationMenuViewport data-slot="navigation-menu-viewport" v-bind="forwardedProps" :class="
         cn(
-          'peer origin-top bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:mt-0 data-[state=open]:fade-in-0 data-[state=open]:mt-px relative mt-5 h-[var(--reka-navigation-menu-viewport-height)] w-full overflow-hidden rounded-b-xl shadow-xl shadow-slate-600/20 duration-200',
+          'peer origin-top bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:mt-0 data-[state=open]:fade-in-0 data-[state=open]:mt-0 relative mt-5 h-[var(--reka-navigation-menu-viewport-height)] w-full overflow-hidden rounded-b-xl shadow-xl shadow-slate-600/20 duration-200',
           props.class,
         )
       " />
