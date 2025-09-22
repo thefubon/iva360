2085657adfe3e8c6389c5c1a361d860faead0377
Fubon - Website Design & Frontend Development <43171309+thefubon@users.noreply.github.com>
2025-09-22 11:37:14 +0300

    upd

--- app/components/dashboard/DashboardHeader.vue
+++ app/components/dashboard/DashboardHeader.vue
@@ -25,13 +25,41 @@ const isMobile = breakpoints.smaller('mobile');
     <div class="flex items-center md:gap-4 gap-3">
       <Popover>
         <PopoverTrigger asChild>
-          <Button variant="outlineDark" :size="isMobile ? 'icon' : 'default'">
+          <Button variant="outlineDark" :size="isMobile ? 'icon' : 'default'" class="cursor-pointer">
             <MonitorSmartphone class="size-5 stroke-[1.5]" /> <span class="hidden md:block">Приложение</span>
           </Button>
         </PopoverTrigger>
 
-        <PopoverContent side="bottom" align="end" :sideOffset="8" class="border-none shadow-2xl shadow-slate-600/15 rounded-xl" asChild>
-          Some popover content
+        <PopoverContent side="bottom" align="end" :sideOffset="8"
+          class="border-none shadow-2xl shadow-slate-600/15 rounded-xl min-w-[600px] p-0 overflow-hidden">
+          <div class="flex items-center justify-between gap-6">
+            <div class="space-y-4 p-6 pr-0 flex-1">
+              <div>
+                <p class="text-lg font-semibold">Мобильное приложение</p>
+                <p class="text-sm text-secondary">Управляйте вашим мероприятием в&nbsp;любое время и&nbsp;в&nbsp;любом
+                  месте
+                </p>
+              </div>
+
+              <div class="flex gap-x-2">
+                <NuxtLink href="#" target="_blank">
+                  <NuxtImg class="h-9" src="/img/Apple.svg" alt="iOS" />
+                </NuxtLink>
+
+                <NuxtLink href="#" target="_blank">
+                  <NuxtImg class="h-9" src="/img/Android.svg" alt="Android" />
+                </NuxtLink>
+              </div>
+            </div>
+
+            <div>
+              <NuxtImg class="h-28 p-1 border border-border rounded-lg block" src="/img/QRCode.svg" alt="QR-Code" />
+            </div>
+
+            <div>
+              <NuxtImg class="h-full w-full block" src="/img/DownloadImages.png" alt="" />
+            </div>
+          </div>
         </PopoverContent>
       </Popover>
 
