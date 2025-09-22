bc1f421a18b3299de9654c30cb3378bbe00ddbd9
Fubon - Website Design & Frontend Development <43171309+thefubon@users.noreply.github.com>
2025-09-22 12:36:19 +0300

    upd

--- app/components/dashboard/Footer.vue
+++ app/components/dashboard/Footer.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="p-4">
-    Footer
+  <div class="p-4 flex justify-between items-center text-sm text-secondary">
+    <div>
+      IVA 360 ® 2021-2025
+    </div>
+
+    <div>
+      Контакты / AI Ассистент
+    </div>
   </div>
 </template>
