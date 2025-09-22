cae32571ccac0ab2a60e59fb375c79062ff32112
Fubon - Website Design & Frontend Development <43171309+thefubon@users.noreply.github.com>
2025-09-22 13:04:58 +0300

    upd

--- app/components/dashboard/SidebarNav.vue
+++ app/components/dashboard/SidebarNav.vue
@@ -8,7 +8,7 @@ import {
 } from "@/components/ui/sidebar"
 import { Separator } from "@/components/ui/separator"
 
-import { LayoutPanelLeft, Users, BriefcaseBusiness, Blocks, FileCode2, ChartBarStacked, CreditCard, MessageCircleQuestionMark, BookMarked, ListTodo, SquareLibrary } from "lucide-vue-next"
+import { LayoutPanelLeft, Users, BriefcaseBusiness, Blocks, FileCode2, ChartBarStacked, CreditCard, MessageCircleQuestionMark, BookMarked, ListTodo, SquareLibrary, SquareArrowOutUpRight } from "lucide-vue-next"
 import Meetings from "../icons/sidebar/Meetings.vue";
 import Messenger from "../icons/sidebar/Messenger.vue";
 import Calendar from "../icons/sidebar/Calendar.vue";
@@ -21,21 +21,25 @@ const items = [
     title: "Встречи",
     url: "#",
     icon: Meetings,
+    target: false,
   },
   {
     title: "Мессенджер",
-    url: "#",
+    url: "https://meet.iva360.ru",
     icon: Messenger,
+    target: true,
   },
   {
     title: "Почта и Календарь",
     url: "#",
     icon: Calendar,
+    target: false,
   },
   {
     title: "Диск и Документы",
     url: "#",
     icon: Disc,
+    target: false,
   },
 ];
 
@@ -44,36 +48,43 @@ const items2 = [
     title: "Дашборд",
     url: "/dashboard",
     icon: LayoutPanelLeft,
+    target: false,
   },
   {
     title: "Пользователи",
     url: "#",
     icon: Users,
+    target: false,
   },
   {
     title: "Моя компания",
     url: "#",
     icon: BriefcaseBusiness,
+    target: false,
   },
   {
     title: "Мои продукты",
     url: "#",
     icon: Blocks,
+    target: false,
   },
   {
     title: "Управление API",
     url: "#",
     icon: FileCode2,
+    target: false,
   },
   {
     title: "Статистика",
     url: "#",
     icon: ChartBarStacked,
+    target: false,
   },
   {
     title: "Тарифы",
     url: "#",
     icon: CreditCard,
+    target: false,
   },
 ];
 
@@ -82,21 +93,25 @@ const items3 = [
     title: "Подсказки",
     url: "#",
     icon: MessageCircleQuestionMark,
+    target: false,
   },
   {
     title: "База знаний",
-    url: "#",
+    url: "https://help.iva360.ru",
     icon: BookMarked,
+    target: true,
   },
   {
     title: "Дорожная карта",
     url: "#",
     icon: ListTodo,
+    target: false,
   },
   {
     title: "Документы платформы",
     url: "#",
     icon: SquareLibrary,
+    target: false,
   },
 ];
 </script>
@@ -111,9 +126,11 @@ const items3 = [
               '!h-9 px-2 hover:bg-muted font-medium',
               route.path === item.url ? 'bg-muted' : ''
             ]" asChild>
-              <NuxtLink :href="item.url">
+              <NuxtLink :href="item.url" :target="item.target ? '_blank' : undefined">
                 <component :is="item.icon" class="!size-6" />
-                <span>{{ item.title }}</span>
+                <div class="flex items-center gap-2">{{ item.title }}
+                  <SquareArrowOutUpRight v-if="item.target" class="!size-4 ml-auto" />
+                </div>
               </NuxtLink>
             </SidebarMenuButton>
           </SidebarMenuItem>
@@ -131,9 +148,11 @@ const items3 = [
                 '!h-9 px-2 hover:bg-muted',
                 route.path === item.url ? 'bg-muted' : ''
               ]" asChild>
-              <NuxtLink :href="item.url">
+              <NuxtLink :href="item.url" :target="item.target ? '_blank' : undefined">
                 <component :is="item.icon" class="!size-6 stroke-[1.5]" />
-                <span>{{ item.title }}</span>
+                <div class="flex items-center gap-2">{{ item.title }}
+                  <SquareArrowOutUpRight v-if="item.target" class="!size-4 ml-auto" />
+                </div>
               </NuxtLink>
             </SidebarMenuButton>
           </SidebarMenuItem>
@@ -151,9 +170,11 @@ const items3 = [
                 '!h-9 px-2 hover:bg-muted',
                 route.path === item.url ? 'bg-muted' : ''
               ]" asChild>
-              <NuxtLink :href="item.url">
+              <NuxtLink :href="item.url" :target="item.target ? '_blank' : undefined">
                 <component :is="item.icon" class="!size-6 stroke-[1.5]" />
-                <span>{{ item.title }}</span>
+                <div class="flex items-center gap-2">{{ item.title }}
+                  <SquareArrowOutUpRight v-if="item.target" class="!size-4 ml-auto" />
+                </div>
               </NuxtLink>
             </SidebarMenuButton>
           </SidebarMenuItem>
