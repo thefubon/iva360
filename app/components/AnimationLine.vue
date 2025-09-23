2dd8c4721564d2eecddf9c261d9bffbecfee4d51
Fubon - Website Design & Frontend Development <43171309+thefubon@users.noreply.github.com>
2025-09-23 11:31:46 +0300

    upd

--- app/components/AnimationLine.vue
+++ app/components/AnimationLine.vue
@@ -3,7 +3,7 @@
     <svg class="wave" width="2305" height="1420" viewBox="0 0 2305 1420" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path ref="wavePathRef" class="wave-path"
         d="M0.544312 30.5231C354.321 30.5231 809.022 316.137 823.675 718.029C841.99 1220.39 308.022 1371.95 266.666 1088.6C225.31 805.255 621.912 643.75 1116.41 735.647C1422.69 792.566 1541.95 980.944 1575.83 1040.57C1622.88 1123.38 1696.44 1165.25 1845.52 1105.62C2000.88 1043.48 2157.22 1090.72 2164.97 1280.32C2168.02 1354.92 2199.81 1389.47 2304.04 1389.47"
-        stroke="url(#paint0_linear_29_1196)" stroke-width="40" />
+        stroke="url(#paint0_linear_29_1196)" stroke-width="60" />
       <defs>
         <linearGradient id="paint0_linear_29_1196" x1="0.544302" y1="80.6013" x2="2274.84" y2="1356.06"
           gradientUnits="userSpaceOnUse">
