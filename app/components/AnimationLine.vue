4a2aa649b3ab2107a3a58991392893ad7a92611b
Fubon - Website Design & Frontend Development <43171309+thefubon@users.noreply.github.com>
2025-09-26 12:08:35 +0300

    upd

--- app/components/AnimationLine.vue
+++ app/components/AnimationLine.vue
@@ -1,17 +1,19 @@
 <template>
-  <div class="overflow-x-clip absolute left-0 w-full h-auto pointer-events-none -z-10" ref="containerRef">
-    <svg class="wave" width="2305" height="1420" viewBox="0 0 2305 1420" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <div class="overflow-x-clip absolute top-280 left-0 w-full h-auto pointer-events-none -z-10 scale-150" ref="containerRef">
+    <svg class="wave" width="4392" height="10759" viewBox="0 0 4392 10759" fill="none"
+      xmlns="http://www.w3.org/2000/svg">
       <path ref="wavePathRef" class="wave-path"
-        d="M0.544312 30.5231C354.321 30.5231 809.022 316.137 823.675 718.029C841.99 1220.39 308.022 1371.95 266.666 1088.6C225.31 805.255 621.912 643.75 1116.41 735.647C1422.69 792.566 1541.95 980.944 1575.83 1040.57C1622.88 1123.38 1696.44 1165.25 1845.52 1105.62C2000.88 1043.48 2157.22 1090.72 2164.97 1280.32C2168.02 1354.92 2199.81 1389.47 2304.04 1389.47"
-        stroke="url(#paint0_linear_29_1196)" stroke-width="60" />
+        d="M3463.5 656.501C3195.33 609.334 1996.5 326.5 2180 71.9999C2357.37 -174 3713 855 3264 1178C3031.02 1345.6 2508.01 962.806 1576 886C854 826.5 -0.768311 928.471 291.5 1546C668.001 2341.5 1700.42 2475.16 3089 2552.5C4660 2640 4031 3775 3264 4006.5C2697.62 4177.44 1479.81 3838.59 1670 3492.5C1878 3114 2948.99 4019.23 2999 4733.51C3034.5 5240.51 3461.7 5372.28 3828.5 5395.01C4692 5448.51 4281.5 6274.51 4093.5 6524.01C3685.63 7065.31 1167 9226.99 321.502 8704.99C-221.838 8369.53 267.001 7353.01 1167 7827.01C2067 8301.01 2160.5 9205.49 1680.5 9765.49C1206.09 10319 519 10580.5 34 10724.5"
+        stroke="url(#paint0_linear_4359_47401)" stroke-width="68" stroke-linecap="round" stroke-linejoin="round" />
       <defs>
-        <linearGradient id="paint0_linear_29_1196" x1="0.544302" y1="80.6013" x2="2274.84" y2="1356.06"
+        <linearGradient id="paint0_linear_4359_47401" x1="700.999" y1="34.3873" x2="7837.21" y2="3446.8"
           gradientUnits="userSpaceOnUse">
-          <stop stop-color="#00B08B" />
-          <stop offset="1" stop-color="#80DEC8" />
+          <stop stop-color="#0AAE6A" />
+          <stop offset="1" stop-color="#15D2CB" />
         </linearGradient>
       </defs>
     </svg>
+
   </div>
 </template>
 
@@ -67,9 +69,9 @@ const initAnimation = () => {
     ease: "none",
     scrollTrigger: {
       trigger: container,
-      start: "top bottom", // Начинаем когда верх контейнера достигает низа экрана
-      end: "bottom center", // Заканчиваем когда низ контейнера в центре экрана
-      scrub: 1
+      start: "top right", // Начинаем когда верх контейнера достигает низа экрана
+      end: "bottom left", // Заканчиваем когда низ контейнера в центре экрана
+      scrub: 0
     }
   })
 }
@@ -77,10 +79,12 @@ const initAnimation = () => {
 
 <style scoped>
 .wave {
-  width: 100%;
+  width: 120%;
   height: auto;
-  width: calc(100% + 14.4px); /* 0.3 * 48px */
-  margin-left: calc(-14.4px); /* -0.3 * 48px */
+  width: calc(100% + 110px);
+  /* 0.3 * 48px */
+  margin-left: calc(110px);
+  /* -0.3 * 48px */
 }
 
 .wave-path {
@@ -92,8 +96,10 @@ const initAnimation = () => {
 /* Адаптация для мобильных устройств */
 @media (max-width: 768px) {
   .wave {
-    width: calc(100% + 480px); /* 10 * 48px */
-    margin-left: calc(-14.4px); /* -0.3 * 48px */
+    width: calc(100% + 110px);
+    /* 10 * 48px */
+    margin-left: calc(110px);
+    /* -0.3 * 48px */
   }
 }
 </style>
